cce2fdcb4adc96925fca9885a4a81dbac632fe40
JonathonHowland <howlandjonathon@gmail.com>
2020-10-26 16:10:58 -0500

    Updated SQL queries

--- FinalProject.sql
+++ FinalProject.sql
@@ -1,1 +1,5 @@
-select 
+select major, perchange from degreesthatpay where (perchange >= all (select perchange from degreesthatpay));
+
+select major, mid90 from degreesthatpay where (mid90 >= all (select mid90 from degreesthatpay));
+
+select schoolname, startmed, region from salariesbyregion order by region, startmed desc;
